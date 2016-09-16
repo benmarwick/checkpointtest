eca81f10bca85e7d26d66ca369a73733b47d8bb5
Ben Marwick <benmarwick@gmail.com>
2016-09-16 10:39:40 -0700

    pkg ver obj format

--- tests/testthat/test_pkg_version.R
+++ tests/testthat/test_pkg_version.R
@@ -3,6 +3,6 @@ context("test that we have the right package version")
 library(checkpointtest)
 
 test_that("test that we have the right package version", {
-  expect_equal(packageVersion("MASS"),
-               package_version("7.3.35"))
+  expect_equal(as.character(packageVersion("MASS")),
+               "7.3.35")
 })
