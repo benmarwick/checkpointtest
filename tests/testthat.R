5c823c93c1b1d4ed7688b06728350119156e345c
Ben Marwick <benmarwick@gmail.com>
2016-09-16 09:13:29 -0700

    add test

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(checkpointtest)
+
+test_check("checkpointtest")
---
+++ tests/testthat/test_pkg_version.R
@@ -0,0 +1,3 @@
+context("test that we have the right package version")
+
+test_that(packageVersion("MASS") == "7.3.35")
